d3afa203c295235afe0e59ce92f5cf3891c7f5c4
hildamu <mukamihilda437@gmail.com>
2024-04-02 13:17:59 +0300

    submit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let mainElement = document.getElementById('main');
+if (mainElement !==null){
+    mainElement.parentNode.removeChild(mainElement);
+
+}
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Hilda is the champion";
